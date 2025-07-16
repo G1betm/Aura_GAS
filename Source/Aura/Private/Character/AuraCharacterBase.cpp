6f1b193a3fcd95012fc19d1decd36fadb963042b
G1betm <aahmedov.gibe@gmail.com>
2025-07-16 11:01:52 +0500

    added Ghoul enemy and miltiple attack montages

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -75,7 +75,7 @@ FVector AAuraCharacterBase::GetCombatSocketLocation_Implementation(const FGamepl
 	}
 	if (MontageTag.MatchesTagExact(GameplayTags.Montage_Attack_LeftHand))
 	{
-		return Weapon->GetSocketLocation(LeftHandSocketName);
+		return GetMesh()->GetSocketLocation(LeftHandSocketName);
 	}
 
 	return FVector();
